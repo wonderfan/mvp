0ef378a7251db592c2c79ebbdb8e388a6960c0cc
wonderfan <fanjiahe2000@163.com>
2019-01-07 01:24:31 +0000

    refine the codes

--- app/mq-client.js
+++ app/mq-client.js
@@ -25,13 +25,13 @@ module.exports.consume = async function(){
             let txString = msg.content.toString();
             let tx = JSON.parse(txString);
             try{
-                await exports.sleep(1000);
+                //await exports.sleep(1000);
                 let txid = await invoke.invokeChaincode(tx.peers, tx.channelName, tx.chaincodeName, tx.fcn, tx.args, tx.username, tx.orgname);
                 let result = {uid:tx.uid,txid:txid};
                 if(global.socket) global.socket.send(JSON.stringify(result));
             }catch(err){
                 logger.error(err);
-                let result = {uid:tx.uid,txid:err.message};
+                let result = {uid:tx.uid,txid:"invalid transaction because of mvcc conflicts"};
                 if(global.socket) global.socket.send(JSON.stringify(result));
             }
             channel.ack(msg);
